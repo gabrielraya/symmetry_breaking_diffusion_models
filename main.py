3de65e3f1de9fcfeccb02b5eac812291fa562d33
gaboraya@gmail.com <gaboraya@gmail.com>
2023-05-26 12:00:12 +0200

    update format in main

--- main.py
+++ main.py
@@ -31,96 +31,52 @@ flags.DEFINE_integer('grid_size', 200, 'Grid size for alpha axis for visualizati
 flags.mark_flags_as_required(["workdir", "config", "mode"])
 
 
-def setup_logger():
-    """Configure the logger to output to both console and file."""
-    os.makedirs(FLAGS.workdir, exist_ok=True)
-    gfile_stream = open(os.path.join(FLAGS.workdir, 'stdout.txt'), 'w')
-    handler = logging.StreamHandler(gfile_stream)
-    formatter = logging.Formatter('%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger = logging.getLogger()
-    logger.addHandler(handler)
-    logger.setLevel('INFO')
-
-
-def run_training():
-    """Run the training pipeline."""
-    run_lib.train(FLAGS.config, FLAGS.workdir)
-
-
-def run_evaluation():
-    """Run the evaluation pipeline."""
-    run_lib.evaluate(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder)
-
-
-def generate_fid_stats():
-    """Generate moment statistics mu and cov for FID scores."""
-    ffid.fid_stats(FLAGS.config)
-
-
-def generate_gauss_approx_stats():
-    """Generate moment statistics mu and cov for Gaussian approximation."""
-    fs.gauss_approx_stats(FLAGS.config)
-
-
-def run_ablation_study():
-    """Run ablation study for linear samplers."""
-    fs.ablation(FLAGS.config, FLAGS.workdir)
-
-
-def run_correlation_analysis():
-    """Run correlation analysis and plot the normalized pixels."""
-    run_lib.correlation_analysis(FLAGS.config, FLAGS.workdir)
-
-
-def run_late_sampler_ablation():
-    """Run ablation over trained model starting at different diffusion steps."""
-    fs.evaluate_fids(FLAGS.config, FLAGS.workdir)
-
-
-def run_given_sampler_ablation():
-    """Run a given sampler."""
-    fs.evaluate_fids_fast_samplers(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder,
-                                   FLAGS.n_steps, FLAGS.fast_sampler,
-                                   gaussian_approximation=FLAGS.gaussian_approximation)
-
-
-def visualize_given_sampler():
-    """Run a given sampler for visualization."""
-    fs.visualize_analysis(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder, N=FLAGS.samples)
-
-
-def visualize_potentials():
-    """Visualize the potentials."""
-    fs.visualize_potentials(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder, FLAGS.generate, FLAGS.grid_size)
-
-
 def main(argv):
-    """Main function."""
-    setup_logger()
-
     if FLAGS.mode == "train":
-        run_training()
+        # Create the working directory
+        os.makedirs(FLAGS.workdir, exist_ok=True)
+        # Set logger so that it outputs to both console and file
+        gfile_stream = open(os.path.join(FLAGS.workdir, 'stdout.txt'), 'w')
+        handler = logging.StreamHandler(gfile_stream)
+        formatter = logging.Formatter('%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger = logging.getLogger()
+        logger.addHandler(handler)
+        logger.setLevel('INFO')
+        # Run the training pipeline
+        run_lib.train(FLAGS.config, FLAGS.workdir)
     elif FLAGS.mode == "eval":
-        run_evaluation()
+        # Run the evaluation pipeline
+        run_lib.evaluate(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder)
     elif FLAGS.mode == "fid_stats":
-        generate_fid_stats()
+        # Generate moment statistics mu and cov for fid scores
+        ffid.fid_stats(FLAGS.config)
     elif FLAGS.mode == "gauss_approx_stats":
-        generate_gauss_approx_stats()
+        # Generate moment statistics mu and cov for gaussian approximation
+        fs.gauss_approx_stats(FLAGS.config)
     elif FLAGS.mode == "ablation":
-        run_ablation_study()
+        # Run ablation study for linear samplers
+        fs.ablation(FLAGS.config, FLAGS.workdir)
     elif FLAGS.mode == "correlation":
-        run_correlation_analysis()
+        # Run correlation analysis and plot the normalized pixels
+        run_lib.correlation_analysis(FLAGS.config, FLAGS.workdir)
     elif FLAGS.mode == "late_sampler":
-        run_late_sampler_ablation()
+        # Run ablation over trained model starting at different diffusion steps.
+        fs.evaluate_fids(FLAGS.config, FLAGS.workdir)
     elif FLAGS.mode == "ablation_fast_sampler":
-        run_given_sampler_ablation()
+        # Run a given sampler
+        fs.evaluate_fids_fast_samplers(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder,
+                                       FLAGS.n_steps, FLAGS.fast_sampler,
+                                       gaussian_approximation=FLAGS.gaussian_approximation)
     elif FLAGS.mode == "visualize_fast_sampler":
-        visualize_given_sampler()
+        # Run a given sampler
+        fs.visualize_analysis(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder, N=FLAGS.samples)
+
     elif FLAGS.mode == "visualize_potentials":
-        visualize_potentials()
+        # fs.visualize_potentials_avg(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder, FLAGS.generate, FLAGS.grid_size)
+        fs.visualize_potentials(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder, FLAGS.generate, FLAGS.grid_size)
     else:
-        raise ValueError(f"Mode '{FLAGS.mode}' not recognized.")
+        raise ValueError(f"Mode {FLAGS.mode} not recognize.")
 
 
 if __name__ == '__main__':
